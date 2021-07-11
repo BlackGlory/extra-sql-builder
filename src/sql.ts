3192ed039de8f8e19de9648909b71ed8d1fe8d7f
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-07-11 21:05:59 +0000

    feat: improve sql

--- src/sql.ts
+++ src/sql.ts
@@ -1,13 +1,13 @@
 import { isntFalsy, isString } from '@blackglory/types'
 import { Falsy } from 'justypes'
-import { removeFalsyValues, concat } from 'extra-tags'
+import { removeFalsyValues, dedent } from 'extra-tags'
 
 export function sql(...fragments: Array<string | Falsy>): string
 export function sql(strings: TemplateStringsArray, ...values: unknown[]): string
 export function sql(...args: unknown[]): string {
   if (isTemplateStringArray(args[0])) {
     const [strings, ...values] = args
-    return concat(...removeFalsyValues(strings, ...values))
+    return dedent(...removeFalsyValues(strings, ...values))
   } else {
     return `${args.filter(isntFalsy).join('\n')};`
   }
