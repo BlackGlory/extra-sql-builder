c7377f2dcc20052314cf030681c746e2a4d4b0da
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-07-11 21:03:44 +0000

    chore: upgrade dependencies

--- src/sql.ts
+++ src/sql.ts
@@ -1,13 +1,13 @@
 import { isntFalsy, isString } from '@blackglory/types'
 import { Falsy } from 'justypes'
-import { removeFalsyValues, toString } from 'extra-tags'
+import { removeFalsyValues, concat } from 'extra-tags'
 
 export function sql(...fragments: Array<string | Falsy>): string
 export function sql(strings: TemplateStringsArray, ...values: unknown[]): string
 export function sql(...args: unknown[]): string {
   if (isTemplateStringArray(args[0])) {
     const [strings, ...values] = args
-    return toString(...removeFalsyValues(strings, ...values))
+    return concat(...removeFalsyValues(strings, ...values))
   } else {
     return `${args.filter(isntFalsy).join('\n')};`
   }
