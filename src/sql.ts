b2f22ae003e1c9404b09a75b809a83b0597a7284
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-12-14 15:04:49 +0000

    chore: upgrade dependencies

--- src/sql.ts
+++ src/sql.ts
@@ -1,13 +1,12 @@
-import { isArray, isntFalsy, isString } from '@blackglory/prelude'
-import { Falsy } from '@blackglory/prelude'
-import { removeFalsyValues, dedent } from 'extra-tags'
+import {  Falsy, isArray, isntFalsy, isString } from '@blackglory/prelude'
+import { filter, dedent } from 'extra-tags'
 
 export function sql(...fragments: Array<string | Falsy>): string
 export function sql(strings: TemplateStringsArray, ...values: unknown[]): string
 export function sql(...args: unknown[]): string {
   if (isTemplateStringArray(args[0])) {
     const [strings, ...values] = args
-    return dedent(...removeFalsyValues(strings, ...values))
+    return dedent(...filter(isntFalsy, strings, ...values))
   } else {
     return `${args.filter(isntFalsy).join('\n')};`
   }
