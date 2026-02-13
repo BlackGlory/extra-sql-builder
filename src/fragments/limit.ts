ef20349d4c8ce001135a65c3458ffde9560bad93
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2026-02-13 08:23:36 +0000

    fix(offset, limit): error messages

--- src/fragments/limit.ts
+++ src/fragments/limit.ts
@@ -2,8 +2,9 @@ import { assert } from '@blackglory/prelude'
 import { isntNegative } from '@src/utils'
 
 export function LIMIT(limit: number): string {
-  assert(Number.isInteger(limit), 'limit should be an integer')
-  assert(isntNegative(limit), 'limit should be positive')
+  assert(
+    Number.isInteger(limit) && isntNegative(limit)
+  , 'limit should be a non-negative integer')
 
   return `LIMIT ${limit}`
 }
--- src/fragments/offset.ts
+++ src/fragments/offset.ts
@@ -2,8 +2,10 @@ import { assert } from '@blackglory/prelude'
 import { isntNegative } from '@src/utils'
 
 export function OFFSET(offset: number): string {
-  assert(Number.isInteger(offset), 'offset should be an integer')
-  assert(isntNegative(offset), 'offset should be positive')
+  assert(
+    Number.isInteger(offset) && isntNegative(offset)
+  , 'offset should be a non-negative integer'
+  )
 
   return `OFFSET ${offset}`
 }
